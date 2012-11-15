3ff4c89b7718c1cd3788b3fde8d9e3bf970f52ba
dbc211 <dbc211@gmail.com>
2012-11-15 05:06:45 +0000

    Buildings can train units from a queue now.

--- RTS/Assets/Scripts/BuildingControl.cs
+++ RTS/Assets/Scripts/BuildingControl.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 // Parent class for all building controls. Contains general building utility functions
-public class BuildingControl : Selectable {
-
-	// Use this for initialization
-	void Start () {
+public abstract class BuildingControl : Selectable {
 	
+	// Used for keeping track of training in this building
+	private Queue<TrainingTask> unitQueue = new Queue<TrainingTask>();
+	private float unitTimer = 0;
+	private Queue<TrainingTask> techQueue = new Queue<TrainingTask>();
+	private float techTimer = 0;
+	
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	protected override void Update () {
+		base.Update();
+		if(unitQueue.Count > 0) {
+			unitTimer += Time.deltaTime;
+			if(unitTimer >= unitQueue.Peek().TrainingTime)
+				CompleteUnit();
+		}
+		if(techQueue.Count > 0) {
+			techTimer += Time.deltaTime;
+			if(techTimer >= techQueue.Peek().TrainingTime)
+				CompleteTech();
+		}
+	}
 	
+	protected void TrainUnit(TrainingTask trainingTask) {
+		unitQueue.Enqueue(trainingTask);
+	}
+	
+	protected void ResearchTech(TrainingTask trainingTask) {
+		techQueue.Enqueue(trainingTask);
+	}
+	
+	void CompleteUnit() {
+		float distance = this.collider.bounds.size.magnitude + unitQueue.Peek().TrainingObject.collider.bounds.size.magnitude;
+		Instantiate(unitQueue.Peek().TrainingObject, transform.position + (transform.right * distance), Quaternion.identity);
+		unitQueue.Dequeue();
+		unitTimer = 0;
+	}
+	
+	void CompleteTech() {
+		Instantiate(techQueue.Peek().TrainingObject, transform.position, Quaternion.identity);
+		techQueue.Dequeue();
+		techTimer = 0;
 	}
 }
--- RTS/Assets/Scripts/Selectable.cs
+++ RTS/Assets/Scripts/Selectable.cs
@@ -4,30 +4,24 @@ using System.Collections;
 // Defines the behavior of a selectable GameObject
 public class Selectable : MonoBehaviour {
 	
-	public GameObject selectionMarker;
-	
-	private GameObject currentMarker;
-	
 	// Use this for initialization
-	void Start () {
+	protected virtual void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	protected virtual void Update () {
 		
 	}
 	
 	// Called when this GameObject has been selected
 	public void Select() {
-		currentMarker = (GameObject)Instantiate(selectionMarker, this.gameObject.transform.position, Quaternion.identity);
-		currentMarker.transform.parent = this.gameObject.transform;
+		//play some audio file or something
 	}
 	
 	// Called when this GameObject has been deselected
 	public void Deselect() {
-		Destroy(currentMarker);
-		currentMarker = null;
+		
 	}
 	
 	// Called when this GameObject is selected and a key is pressed
