9053c9bec18170d40235bd45e3b4da5db7ec9d8f
dbc211 <dbc211@gmail.com>
2013-07-22 22:11:31 +0000

    Yet more work on rebuilding water networks when nodes are added or removed. Note to self: if something operates on all of the objects within a trigger, it's MUCH easier to determine those objects on the fly than to try and keep track of them as they enter and leave the trigger.

--- RTS/Assets/Scripts/Resources/WaterNetworkNodeTrigger.cs
+++ RTS/Assets/Scripts/Resources/WaterNetworkNodeTrigger.cs
@@ -15,22 +15,30 @@ public class WaterNetworkNodeTrigger : MonoBehaviour {
 	
 	public void OnTriggerEnter(Collider other) {
 		//TODO when a new unit appears in range, this event doesn't fire
-		if(other.name.Equals("WaterNetworkNode")) {
-			WaterNetworkNode.NodeEnteredRange(other.transform.parent.GetComponent<WaterNetworkNode>());
-		} else if(IsSuppliableWithSameOwner(other)) {
-			WaterNetworkNode.SuppliableEnteredRange(other.GetComponent<UnitStatus>());
+		if(IsControllableWithSameOwner(other)) {
+			if(other.name.Equals("WaterNetworkNode")) {
+				WaterNetworkNode.NodeEnteredRange(other.transform.parent.GetComponent<WaterNetworkNode>());
+			} else if(IsSuppliable(other)) {
+				WaterNetworkNode.SuppliableEnteredRange(other.GetComponent<UnitStatus>());
+			}
 		}
 	}
 	
 	public void OnTriggerExit(Collider other) {
-		if(other.name.Equals("WaterNetworkNode")) {
-			WaterNetworkNode.NodeLeftRange(other.transform.parent.GetComponent<WaterNetworkNode>());
-		} else if(IsSuppliableWithSameOwner(other)) {
-			WaterNetworkNode.SuppliableLeftRange(other.GetComponent<UnitStatus>());
+		if(IsControllableWithSameOwner(other)) {
+			if(other.name.Equals("WaterNetworkNode")) {
+				WaterNetworkNode.NodeLeftRange(other.transform.parent.GetComponent<WaterNetworkNode>());
+			} else if(IsSuppliable(other)) {
+				WaterNetworkNode.SuppliableLeftRange(other.GetComponent<UnitStatus>());
+			}
 		}
 	}
 	
-	protected bool IsSuppliableWithSameOwner(Collider other) {
-		return other.GetComponent<Controllable>() != null && other.GetComponent<Controllable>().Owner == Controllable.Owner && other.GetComponent<UnitStatus>() != null;
+	protected bool IsControllableWithSameOwner(Collider other) {
+		return other.GetComponent<Controllable>() != null && other.GetComponent<Controllable>().Owner == Controllable.Owner;
+	}
+	
+	protected bool IsSuppliable(Collider other) {
+		return other.GetComponent<UnitStatus>() != null;
 	}
 }
