267100a4450e5b5d16b499e66fe90ed96079a3f4
dbc211@gmail.com <dbc211@gmail.com@c90b5b1b-634f-9ea1-b47f-ae34b68035cb>
2013-10-09 22:20:48 +0000

    Removing RTS backup from repo. Keeping on local machine (I forget what's in the backup).

--- trunk/RTS - Backup/Assets/Scripts/Player/Team.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-using System;
-
-public class Team : MonoBehaviour {
-	
-	[NonSerialized]
-	public List<Player> players = new List<Player>();
-}
