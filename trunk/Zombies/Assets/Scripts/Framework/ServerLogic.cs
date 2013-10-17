7d8456d6daf18f7fb5e1fa7fe7457e7dfa42f900
dbc211@gmail.com <dbc211@gmail.com@c90b5b1b-634f-9ea1-b47f-ae34b68035cb>
2013-10-17 22:15:33 +0000

    Have a zombie being instantiated over the network for clients that join! It's state should also be synched and smoothed but have yet to test actual movement...

---
+++ trunk/Zombies/Assets/Scripts/Framework/ServerLogic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using uLink;
+
+public class ServerLogic : uLink.MonoBehaviour {
+	
+	public GameObject creatorPrefab;
+	public GameObject proxyPrefab;
+
+	protected void uLink_OnServerInitialized() {
+		uLink.Network.Instantiate(uLink.Network.AllocateViewID(), uLink.NetworkPlayer.server, proxyPrefab, null, creatorPrefab, Vector3.zero, creatorPrefab.transform.rotation, 0);
+	}
+}
